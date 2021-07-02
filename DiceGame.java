eaf3ee66089e03f4581b7052389a9f6ff2db7991
KeigoMinakawa <86813488+KeigoMinakawa@users.noreply.github.com>
2021-07-02 13:57:59 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -29,3 +29,5 @@ System.out.println("Die 2: "+die[1]);
 total = rd.Implement(die[0],die[1]);
 
 System.out.println("Total value: " + total);
+if(total > 7) System.out.println("You won!");
+ else System.out.println("You lost!");
